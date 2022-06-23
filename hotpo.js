5f9f18a6519646a8bb09cbf221d03815b87e3659
JekAnashko <96998019+jjjekkk@users.noreply.github.com>
2022-06-23 23:58:38 +0500

    Add files via upload

---
+++ hotpo.js
@@ -0,0 +1,15 @@
+let hotpo = function(n){
+  if(n == 0) return 0; //Optional Handler to n = 0
+  let sumIteration = 0;
+   do{
+     if (n % 2 == 0) {
+       n = n / 2
+     } else {
+       n = 3*n + 1
+     }
+     sumIteration += 1 
+  }
+  while (n != 1);
+  return sumIteration
+}
+console.log(hotpo(100))
